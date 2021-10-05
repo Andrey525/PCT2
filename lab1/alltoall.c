26ffbbcd9b96f5bc274a40147481e2eb41cf8226
Andrey525 <ghost5run@yandex.ru>
2021-10-05 11:10:44 +0700

    Debug

--- lab1/alltoall.c
+++ lab1/alltoall.c
@@ -10,23 +10,27 @@ int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &commsize);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Request reqs[commsize * commsize * 2];
-    for (int i = 0; i < commsize * commsize * 2; i++) {
-        reqs[i] = MPI_REQUEST_NULL;
-    }
+    MPI_Request reqs[commsize * 2];
     MPI_Status stats[commsize * commsize * 2];
     int count = 1024;
     char *sendbuf = malloc(sizeof(*sendbuf) * count * commsize);
     char *recvbuf = malloc(sizeof(*recvbuf) * count * commsize);
 
+    double t = MPI_Wtime();
     for (int i = 0; i < commsize; i++) {
         MPI_Isend(sendbuf + i * count, count, MPI_CHAR, i, 0, MPI_COMM_WORLD,
-                  &reqs[i + rank * commsize]);
+                  &reqs[i]);
         MPI_Irecv(recvbuf + i * count, count, MPI_CHAR, i, 0, MPI_COMM_WORLD,
-                  &reqs[commsize * commsize + i + rank * commsize]);
+                  &reqs[commsize + i]);
     }
-    MPI_Waitall(commsize * commsize * 2, reqs, stats);
+    MPI_Waitall(commsize * 2, reqs, stats);
 
+    t = MPI_Wtime() - t;
+    double tmax;
+    MPI_Reduce(&t, &tmax, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        printf("Tmax = %lf\n", tmax);
+    }
     free(sendbuf);
     free(recvbuf);
     MPI_Finalize();
