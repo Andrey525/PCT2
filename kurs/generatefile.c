a2c47d24b9766df897785e4aea293e9c2f6ecf3b
Andrey525 <ghost5run@yandex.ru>
2021-11-16 20:33:56 +0700

    Add file read and write

---
+++ kurs/generatefile.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ *	First argument - Name of file
+ *  Second argument - Size
+ */
+
+int main(int argc, char *argv[]) {
+    if (argc == 3) {
+        FILE *fp = fopen(argv[1], "w");
+        if (!fp) {
+            printf("File open failed!\n");
+            printf("Press enter to continue.");
+            getchar();
+            return 0;
+        } else {
+            printf("File for generating random numbers opened successfully!\n");
+        }
+        int size = atoi(argv[2]);
+        srand(time(NULL));
+        for (int i = 0; i < size; i++) {
+            fprintf(fp, "%d\n", rand() % 100);
+        }
+    } else {
+        printf("Use, for example: ./generatefile [Name of file] 100\n");
+        return 1;
+    }
+
+    return 0;
+}
--- kurs/serial.c
+++ kurs/serial.c
@@ -1,72 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define SIZE 100
+#include <time.h>
 
 void quicksort(int *array, int index_left, int index_right) {
 
-	if (index_right == index_left) {
-		return;
-	}
+    if (index_right == index_left) {
+        return;
+    }
 
-	printf("index_left = %d index_right = %d\n", index_left, index_right);
+    int temp;
+    int index_left_for_next = index_left;
+    int index_right_for_next = index_right;
+    int index_pivot = (index_left + index_right) / 2;
 
-	int temp;
-	int index_left_for_next = index_left;
-	int index_right_for_next = index_right;
-	int index_pivot = (index_left + index_right) / 2;
+    while (index_right > index_left) {
+        if (array[index_left] >= array[index_pivot] &&
+            array[index_right] <= array[index_pivot]) {
+            temp = array[index_left];
+            array[index_left] = array[index_right];
+            array[index_right] = temp;
+            if (index_left == index_pivot) {
+                index_pivot = index_right;
+                index_left++;
+            } else if (index_right == index_pivot) {
+                index_pivot = index_left;
+                index_right--;
+            } else {
+                index_left++;
+                index_right--;
+            }
+        } else {
+            if (array[index_left] < array[index_pivot]) {
+                index_left++;
+            }
+            if (array[index_right] > array[index_pivot]) {
+                index_right--;
+            }
+        }
+    }
 
-	while (index_right > index_left) {
-		if (array[index_left] >= array[index_pivot] && array[index_right] <= array[index_pivot]) {
-			temp = array[index_left];
-			array[index_left] = array[index_right];
-			array[index_right] = temp;
-			if (index_left == index_pivot) {
-				index_pivot = index_right;
-				index_left++;
-			} else if (index_right == index_pivot) {
-				index_pivot = index_left;
-				index_right--;
-			} else {
-				index_left++;
-				index_right--;	
-			}		
-		} else {
-			if (array[index_left] < array[index_pivot]) {
-				index_left++;
-			}
-			if (array[index_right] > array[index_pivot]) {
-				index_right--;
-			}
-		}
-
-		for (int i = 0; i < SIZE; i++) {
-			printf("%d ", array[i]);
-		}
-		printf("\n pivot = %d\n", index_pivot);
-
-	}
-	printf("index_left_for_next = %d index_right_for_next = %d\n", index_left_for_next, index_right_for_next);
-	if (index_left_for_next != index_pivot) {
-		quicksort(array, index_left_for_next, index_pivot - 1);	
-	}
-	
-	if (index_right_for_next != index_pivot) {
-		quicksort(array, index_pivot + 1, index_right_for_next);	
-	}
-	
+    if (index_left_for_next != index_pivot) {
+        quicksort(array, index_left_for_next, index_pivot - 1);
+    }
+    if (index_right_for_next != index_pivot) {
+        quicksort(array, index_pivot + 1, index_right_for_next);
+    }
 }
 
 int main(int argc, char *argv[]) {
+    if (argc == 3) {
+        FILE *fp = fopen(argv[1], "r");
+        if (!fp) {
+            printf("File open failed!\n");
+            printf("Press enter to continue.");
+            getchar();
+            return 0;
+        } else {
+            printf("File for reading opened successfully!\n");
+        }
 
-	int array[SIZE];
-	for (int i = 0; i < SIZE; i++) {
-		array[i] = rand() % 100;
-	}
-	int index_left = 0;
-	int index_right = SIZE - 1;
-	
-	quicksort(array, index_left, index_right);
+        int capacity = 10;
+        int i, size;
+        int *array = malloc(sizeof(int) * capacity);
+        for (i = 0; !feof(fp); i++) {
+            if (i == capacity) {
+                capacity = capacity * 2;
+                array = realloc(array, sizeof(int) * capacity);
+            }
+            fscanf(fp, "%d", &array[i]);
+        }
+        fclose(fp);
+        printf("File content read successfully!\n");
+        size = i - 1;
 
-	return 0;
+        printf("Starting to sort!\n");
+        quicksort(array, 0, size - 1);
+        printf("Quicksort completed!\n");
+
+        fp = fopen(argv[2], "w");
+        if (!fp) {
+            printf("File open failed!\n");
+            printf("Press enter to continue.");
+            getchar();
+            return 0;
+        } else {
+            printf("File for writing opened successfully!\n");
+        }
+        for (i = 0; i < size; i++) {
+            fprintf(fp, "%d\n", array[i]);
+        }
+        fclose(fp);
+        printf("Sorted content write to %s successfully!\n", argv[2]);
+
+        free(array);
+    } else {
+        printf("Use: ./serial [Name of source file] [Name of result file]\n");
+    }
+
+    return 0;
 }
